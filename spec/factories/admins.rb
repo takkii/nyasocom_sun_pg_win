8983ede7175244382cd99542337f8e17baa4d754
Takayuki Kamiyama <karuma.reason@gmail.com>
2024-12-20 05:56:52 +0900

    Delete.

--- spec/factories/admins.rb
+++
@@ -1,8 +0,0 @@
-# spec/factories/admins.rb
-FactoryBot.define do
-    factory :admin do
-      sequence(:email) { |n| "karuma_reason#{n}@yahoo.co.jp" }
-      password              { "1122rubyist" }
-      password_confirmation { "1122rubyist" }
-  end
-end
