601f84a79c53e081f4e364b52a4ff04b4a50b4f1
Takayuki Kamiyama <karuma.reason@gmail.com>
2025-01-01 09:39:21 +0900

    Update.

--- app/models/secret.rb
+++ app/models/secret.rb
@@ -4,9 +4,9 @@ class Secret
   def matches?(request)
     current = Date.today
 
-    # 期間指定
-    b_date = Date.parse('2024-1-1')
-    e_date = Date.parse('2024-12-31')
+    # 期間指定、1年単位で更新を任意でする。
+    b_date = Date.parse('2025-1-1')
+    e_date = Date.parse('2025-12-31')
 
     # 期間限定
     (current <=> b_date) >= 0 && (current <=> e_date) <= 0
