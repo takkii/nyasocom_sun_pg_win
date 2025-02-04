6d1002ad578db4010426d56a735dee26a4b2fbbb
Takayuki Kamiyama <karuma.reason@gmail.com>
2025-02-04 11:05:00 +0900

    Update.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,22 +34,11 @@ class ApplicationController < ActionController::Base
        # If you do not resolve the dependent libraries, a runtime error may occur.
        nodejs_path = "node" + " " + "#{File.dirname(__FILE__) + '/myipad.js'}"
        stdout_js, stderr_js, status_js = Open3.capture3(nodejs_path)
+       ip_add = "#{request.env['HTTP_CLIENT_IP']}"
 
-       # IP address(IPv4), enter one digit at a time in ascending order.
-       win_ip_one = ENV['WIN_IP_ONE']
-       win_ip_two = ENV['WIN_IP_TWO']
-       win_ip_three = ENV['WIN_IP_THREE']
-       win_ip_four = ENV['WIN_IP_FOUR']
-       wsl_ip_one = ENV['WSL_IP_ONE']
-       wsl_ip_two = ENV['WSL_IP_TWO']
-       wsl_ip_three = ENV['WSL_IP_THREE']
-       wsl_ip_four = ENV['WSL_IP_FOUR']
-     
         # Development is assumed to be in a local environment.
-        if "#{stdout_js}".match(/#{win_ip_one}.#{win_ip_two}.#{win_ip_three}.#{win_ip_four}/o) || {}[:match]
-          # windows, ip address specification, nothing displayed.
-        elsif "#{stdout_js}".match(/#{wsl_ip_one}.#{wsl_ip_two}.#{wsl_ip_three}.#{wsl_ip_four}/o) || {}[:match]
-          # wsl2, ip address specification, nothing displayed.
+        if "#{stdout_js}".match(/#{ip_add}/o) || {}[:match]
+          # rails side, ip address specification, nothing displayed.
         else 
           # An exception was raised on its own. Something other than an IP address was matched. Please review. 
           render file: "public/404.html", status: :not_found, layout: false
