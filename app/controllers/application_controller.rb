724bef63e24a572557fdd571cc21c484241d8483
Ｔ・Ｋ <karuma.reason@gmail.com>
2025-09-22 04:59:05 +0900

    Update.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -72,16 +72,16 @@ TRUE
 
   def validate_ipaddress
     begin
-     unless Dir.exist?("./app/controllers/node_modules/")
-       yarn_install = "cd #{File.dirname(__FILE__)} && yarn install"
-       stdout_npm, stderr_npm, status_npm = Open3.capture3(yarn_install)
-       stdout_npm
-     end
+      unless Dir.exist?("./app/controllers/node_modules/")
+        yarn_install = "cd #{File.dirname(__FILE__)} && yarn install"
+        stdout_npm, stderr_npm, status_npm = Open3.capture3(yarn_install)
+        stdout_npm
+      end
 
-     # If you do not resolve the dependent libraries, a runtime error may occur.
-     nodejs_path = "node" + " " + "#{File.dirname(__FILE__) + '/myipad.js'}"
-     stdout_js, stderr_js, status_js = Open3.capture3(nodejs_path)
-     ip_add = "#{request.env['HTTP_CLIENT_IP']}"
+      # If you do not resolve the dependent libraries, a runtime error may occur.
+      nodejs_path = "node" + " " + "#{File.dirname(__FILE__) + '/myipad.js'}"
+      stdout_js, stderr_js, status_js = Open3.capture3(nodejs_path)
+      ip_add = "#{request.env['HTTP_CLIENT_IP']}"
 
       # Development is assumed to be in a local environment.
       if "#{stdout_js}".match(/#{ip_add}/o) || {}[:match]
