b5cd38d7af4bdbcfd82c50f1e521ec64d79e0c4b
T・K <karuma.reason@gmail.com>
2025-10-02 09:43:59 +0900

    Update.

--- app/controllers/secret_controller.rb
+++ app/controllers/secret_controller.rb
@@ -11,7 +11,7 @@ class SecretController < BlogsController
     week = %w(日 月 火 水 木 金 土)[dt.wday]
     td = Time.new
     @himekuri = "#{td.year}年" + "#{td.month}月" + "#{td.day}日" + ' : '.to_s + "#{td.hour}時"+"#{td.min}分"+"#{td.sec}秒" + ' : '.to_s + week + "曜日"
-    @version = 3
+    @version = 3.2
     sql = "SHOW pgroonga.libgroonga_version;"
     query = ActiveRecord::Base.connection.select_all(sql).to_a
     pg_string = (query).to_s.gsub(/[^A-Za-z]/, ' ').rstrip
