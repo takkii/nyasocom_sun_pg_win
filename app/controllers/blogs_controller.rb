45c844397ffa978910ec31f991a2b34f2c1dd602
T・K <karuma.reason@gmail.com>
2025-10-06 12:36:29 +0900

    Update.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,8 +3,8 @@
 lib = File.expand_path('lib', __dir__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
+require 'core'
 require 'time'
-require 'version'
 
 class BlogsController < ApplicationController
   before_action :authenticate_user!
@@ -21,14 +21,8 @@ class BlogsController < ApplicationController
     end
     @blogs = Kaminari.paginate_array(Blog.search(params[:query]).order(days: :desc)).page(params[:page])
     @version = CoreNYM.version
-    dt = Time.new.getlocal('+09:00')
-    week = %w(日 月 火 水 木 金 土)[dt.wday]
-    @himekuri = "#{dt.year}年" + "#{dt.month}月" + "#{dt.day}日" + ' : '.to_s + "#{dt.hour}時"+"#{dt.min}分"+"#{dt.sec}秒" + ' : '.to_s + week + "曜日"
-    sql = "SHOW pgroonga.libgroonga_version;"
-    query = ActiveRecord::Base.connection.select_all(sql).to_a
-    pg_string = (query).to_s.gsub(/[^A-Za-z]/, ' ').rstrip
-    pg_number = (query).to_s.gsub(/[^.0-9A-Za-z]/, '').rstrip.delete("A-Za-z").delete_prefix(".").delete_suffix(".")
-    @pg_version = pg_string + " " + pg_number
+    @himekuri = CoreNYM.koyomi
+    @pg_version = CoreNYM.pg_version
 
     respond_to do |format|
       format.html
