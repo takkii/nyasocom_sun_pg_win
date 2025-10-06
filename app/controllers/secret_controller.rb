be63e3551009e7a7a0f51c6db63aa41eae40026e
T・K <karuma.reason@gmail.com>
2025-10-06 12:36:48 +0900

    Update.

--- app/controllers/secret_controller.rb
+++ app/controllers/secret_controller.rb
@@ -3,9 +3,9 @@
 lib = File.expand_path('lib', __dir__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
+require 'core'
 require 'date'
 require 'time'
-require 'version'
 
 class SecretController < BlogsController
   before_action :authenticate_user!
@@ -13,16 +13,9 @@ class SecretController < BlogsController
 
   def index
     @status = Kaminari.paginate_array(Blog.where(status: 'true').order(days: :desc)).page(params[:page]).per(5)
-    dt = Date.today
-    week = %w(日 月 火 水 木 金 土)[dt.wday]
-    td = Time.new
-    @himekuri = "#{td.year}年" + "#{td.month}月" + "#{td.day}日" + ' : '.to_s + "#{td.hour}時"+"#{td.min}分"+"#{td.sec}秒" + ' : '.to_s + week + "曜日"
     @version = CoreNYM.version
-    sql = "SHOW pgroonga.libgroonga_version;"
-    query = ActiveRecord::Base.connection.select_all(sql).to_a
-    pg_string = (query).to_s.gsub(/[^A-Za-z]/, ' ').rstrip
-    pg_number = (query).to_s.gsub(/[^.0-9A-Za-z]/, '').rstrip.delete("A-Za-z").delete_prefix(".").delete_suffix(".")
-    @pg_version = pg_string + " " + pg_number
+    @himekuri = CoreNYM.koyomi
+    @pg_version = CoreNYM.pg_version
   end
 
 private
