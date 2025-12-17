9d83c103f61b02347d5b9d9927d718e853895365
Ｔ・Ｋ <karuma.reason@gmail.com>
2025-12-17 11:10:10 +0900

    Comment.

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# Remove a potentially pre-existing server.pid for Rails.
+# Remove, potentially pre-existing server.pid for Rails.
 rm -f ./tmp/pids/server.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
